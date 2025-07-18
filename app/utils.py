bdbe75db04e601c53b864b2ead49c5deed14fade
Farzam <farzamgt@gmail.com>
2025-07-18 08:39:56 +0300

    Fixed AttributeError in utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,5 +1,6 @@
 def format_price(price: float) -> str:
     rounded_price = round(price, 2)
+    rounded_price = float(rounded_price)
     return (
         str(int(rounded_price))
         if rounded_price.is_integer()
